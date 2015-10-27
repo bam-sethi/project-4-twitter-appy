027c6bbe15dfa29f09e9f807107400eb765395f5
Ambar Sethi <ambarrrsethi@gmail.com>
2015-10-27 20:20:31 +0000

    trying to iterate over live twitter data, and pass to chart, wrote some nice code that doesnt do anything

--- app/js/controller/tweetController.js
+++ app/js/controller/tweetController.js
@@ -9,40 +9,32 @@ function tweetController($scope, $http) {
   self.title = "Twitterappy";
 
   var socket = io('http://localhost:3000');
-  self.tweets = [];
+
+  $scope.data = []
 
   socket.on('tweet', function(tweet) {
-    console.log(tweet);
-    console.log(tweet.sentiment.score)
+    $scope.data.push(tweet.sentiment.score)    
+    var sentiment = $scope.data
+
+    console.log(sentiment)
+    for(var i = -1; i < sentiment.length; i ++ ){
+      if(i < 0){
+        $scope.labels = ["negative"]
+        console.log($scope.labels)
+      } else if( i > 0 ){
+        $scope.labels = ["positive"]
+        console.log($scope.labels)
+      }
+    }
+
   });
+    // $scope.data = [10, 10]
+    // $scope.labels = ["pos", "neg"]
+  //if sentiment.score < 0 
 
+  
 
-
-
-  // self.labels = ["pos", "neg"]
-  // self.data = [100, 400]
 
 
 }
 
-
-
-    // console.log(tweet)
-    // console.log(tweet)
-    // var tweetText = tweet.text.replace(/[^\w\s]/gi, '');
-    // var sentimentAPI = "7f8e61099ff5c865cd5f736e57d76638905b9b0d";
-    // // console.log(tweetText)
-    // // var jsonData = JSON.stringify(tweetText)
-    // var transformedTweet = encodeURI(tweetText);
-    // // console.log(transformedTweet);
-    // var url = "http://www.tweetsentimentapi.com/api/?key=" + sentimentAPI + "&text=" + transformedTweet
-    // // console.log(jsonData)
-    
-    // $http.get(url)
-    //   .success(function(response){
-    //     console.log(response)
-    //     // self.tweets.push(data);
-    //     // $scope.$apply();
-    //     }, function (response){
-    //       console.log(response)
-    // })
