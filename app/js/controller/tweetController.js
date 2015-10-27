f26ac3a16d1fbecc8aa3e2d4e692f2328a0b26cf
Ambar Sethi <ambarrrsethi@gmail.com>
2015-10-27 21:24:14 +0000

    because chartjs

--- app/js/controller/tweetController.js
+++ app/js/controller/tweetController.js
@@ -11,27 +11,51 @@ function tweetController($scope, $http) {
   var socket = io('http://localhost:3000');
 
   $scope.data = []
+  $scope.label = []
 
   socket.on('tweet', function(tweet) {
-    $scope.data.push(tweet.sentiment.score)    
-    var sentiment = $scope.data
+    $scope.data.push(tweet.sentiment.score);    
+    // $scope.sentimentScore = $scope.data;
 
-    console.log(sentiment)
-    for(var i = -1; i < sentiment.length; i ++ ){
-      if(i < 0){
-        $scope.labels = ["negative"]
-        console.log($scope.labels)
-      } else if( i > 0 ){
-        $scope.labels = ["positive"]
-        console.log($scope.labels)
-      }
-    }
-
+    $scope.label.push(tweet.sentiment.sentiment);
+    // $scope.sentimentAnalysis = $scope.label;
+    // console.log($scope.label)
   });
+
+
+
+
+
+    //or something like this?
+  // $scope.data = {
+  //   data: sentimentScore,
+  //   label
+  // }
+
+  //need to basically do this, push the score in to the data array that chart js requires
+  //and push the label in, can i do this as an object
+  //can i make the data that chart recieves an object
+  //where does chart js begin and where does angular-chart-js begin?
+
+  // $scope.data = [tweet.sentiment.score]
+  // $scope.label = [tweet.sentiment.sentiment]
+
+
+
+
     // $scope.data = [10, 10]
     // $scope.labels = ["pos", "neg"]
   //if sentiment.score < 0 
-
+    // console.log(sentiment)
+    // for(var i = -1; i < sentiment.length; i ++ ){
+    //   if(i < 0){
+    //     $scope.labels = ["negative"]
+    //     console.log($scope.labels)
+    //   } else if( i > 0 ){
+    //     $scope.labels = ["positive"]
+    //     console.log($scope.labels)
+    //   }
+    // }
   
 
 
